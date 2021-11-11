dc8bfc5adf6578d55599dfb436991dfefd1e5fbf
OlalalaO <1176328868@qq.com>
2021-11-12 00:06:42 +0800

    try

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@
           <div class="layout-nav">
             <MenuItem name="1">
               <Icon type="ios-navigate"></Icon>
-              Item 1
+              个人中心
             </MenuItem>
             <MenuItem name="2">
               <Icon type="ios-keypad"></Icon>
