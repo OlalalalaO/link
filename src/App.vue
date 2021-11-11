c26147ed7f3749e1db8c546bccf42b9d5ac4277b
OlalalaO <1176328868@qq.com>
2021-11-11 23:57:05 +0800

    feat: init layout

--- src/App.vue
+++ src/App.vue
@@ -57,40 +57,30 @@
             <Submenu name="1">
               <template slot="title">
                 <Icon type="ios-navigate"></Icon>
-                Item 1
+                日报
               </template>
-              <MenuItem name="1-1">Option 1</MenuItem>
-              <MenuItem name="1-2">Option 2</MenuItem>
-              <MenuItem name="1-3">Option 3</MenuItem>
+              <MenuItem name="1-1">每日工作</MenuItem>
+              <MenuItem name="1-2">生成日报</MenuItem>
+              <MenuItem name="1-3">查看日报</MenuItem>
             </Submenu>
             <Submenu name="2">
               <template slot="title">
                 <Icon type="ios-keypad"></Icon>
-                Item 2
+                周报
               </template>
-              <MenuItem name="2-1">Option 1</MenuItem>
-              <MenuItem name="2-2">Option 2</MenuItem>
+              <MenuItem name="2-1">生成周报</MenuItem>
+              <MenuItem name="2-2">查看周报</MenuItem>
             </Submenu>
             <Submenu name="3">
               <template slot="title">
                 <Icon type="ios-analytics"></Icon>
-                Item 3
+                月报
               </template>
-              <MenuItem name="3-1">Option 1</MenuItem>
-              <MenuItem name="3-2">Option 2</MenuItem>
+              <MenuItem name="3-1">生成月报</MenuItem>
+              <MenuItem name="3-2">查看月报</MenuItem>
             </Submenu>
           </Menu>
         </Sider>
-        <Layout :style="{padding: '0 24px 24px'}">
-          <Breadcrumb :style="{margin: '24px 0'}">
-            <BreadcrumbItem>Home</BreadcrumbItem>
-            <BreadcrumbItem>Components</BreadcrumbItem>
-            <BreadcrumbItem>Layout</BreadcrumbItem>
-          </Breadcrumb>
-          <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
-            Content
-          </Content>
-        </Layout>
       </Layout>
     </Layout>
   </div>
