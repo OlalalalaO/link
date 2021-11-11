9a1b104e775ff5ad1893f6803cda41af14948b26
zixuanzhao <zixuan.zhao@yunbu.me>
2021-11-11 19:37:23 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import iView from "view-design";
+import './plugins/iview.js'
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  iView,
   render: h => h(App)
 }).$mount('#app')
